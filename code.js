9f51c24104ebff1f955de5f897957ca9e3f12067
JJ <jc@orbsdigital.com>
2024-01-16 20:42:40 +1100

    set  document.getElementById for tags

---
+++ code.js
@@ -0,0 +1,8 @@
+const computerPickOutput = document.getElementById('computer-choice')
+const userPickOutput = document.getElementById('user-choice')
+const resultOutput = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+let userPick 
+let computerPick 
+let result
+
