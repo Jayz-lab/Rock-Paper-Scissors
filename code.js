5939837338234d901976fb373c4d230e8077adc6
JJ <jc@orbsdigital.com>
2024-01-16 20:53:42 +1100

    initialise code to capture event clicks

--- code.js
+++ code.js
@@ -1,8 +1,15 @@
 const computerPickOutput = document.getElementById('computer-choice')
 const userPickOutput = document.getElementById('user-choice')
 const resultOutput = document.getElementById('result')
-const possibleChoices = document.querySelectorAll('button')
+const possibleChoices = document.querySelectorAll('button')//picks up 'all' button element sets
 let userPick 
 let computerPick 
 let result
 
+//captures user event clicks from possibleChoices
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    userPick = e.target.id 
+    userPickOutput.innerHTML = userPick//updates user's hoice in the interface
+    generateComputerChoice()
+    getResult()
+}))
