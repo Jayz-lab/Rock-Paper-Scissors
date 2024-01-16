7e637d6829111bbcbd586044a991e5bf8c0aa7ad
JJ <jc@orbsdigital.com>
2024-01-16 21:04:32 +1100

    initlise function for comp generate choice

--- code.js
+++ code.js
@@ -10,6 +10,22 @@ let result
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
     userPick = e.target.id 
     userPickOutput.innerHTML = userPick//updates user's hoice in the interface
-    generateComputerChoice()
+    generateComputerChoice() //generate comp choice function
     getResult()
 }))
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random()*3) + 1//or possibleChoices.length
+
+    if (randomNumber === 1) {
+        computerPick = 'rock'
+    }
+    if (randomNumber === 2) {
+        computerPick = 'scissors'
+    }
+    if (randomNumber === 3) {
+        computerPick = 'paper'
+    }
+    
+    computerPickOutput.innerHTML = computerPick
+}
